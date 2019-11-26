b62c7a65fdf15b337643088d0838580498067dc0
Tim Herbst <therbst@worcester.edu>
2019-11-26 11:32:42 -0500

    blank project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,5 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'Simulated Operating System';
-  testdata : String
-  constructor(private http: HttpClient) {}
-  testget() : void {
-  this.http.get<String>('http://localhost:8080/test/8')
-  .subscribe(data => {
-      this.testdata = data;
-      console.log(data);
-  }
-  );
-  }
+
 }
