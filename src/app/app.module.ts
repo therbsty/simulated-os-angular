de61c5f10a28c6609a820e9b43928dd0531d503e
Shams Al-Farees <salfarees@worcester.edu>
2019-12-03 12:11:45 -0500

    fixing the HTML

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { VariableListComponent } from './variable-list/variable-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VariableListComponent
   ],
   imports: [
     BrowserModule,
